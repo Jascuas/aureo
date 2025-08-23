56e399cbeaaad2aae2f66ddde47e22631b5057b7
Javier Sanchez <j.sanchez@ext.douglas.eu>
2025-08-23 16:55:31 +0200

    no

--- eslint.config.js
+++ eslint.config.js
@@ -15,30 +15,30 @@ export default tseslint.config(
       "simple-import-sort": simpleImportSort,
       "unused-imports": unusedImports,
     },
-    rules: {
-      "simple-import-sort/imports": "error",
-      "simple-import-sort/exports": "warn",
+    // rules: {
+    //   "simple-import-sort/imports": "error",
+    //   "simple-import-sort/exports": "warn",
 
-      "unused-imports/no-unused-imports": "error",
-      "unused-imports/no-unused-vars": [
-        "warn",
-        {
-          vars: "all",
-          varsIgnorePattern: "^_",
-          args: "after-used",
-          argsIgnorePattern: "^_",
-        },
-      ],
+    //   "unused-imports/no-unused-imports": "error",
+    //   "unused-imports/no-unused-vars": [
+    //     "warn",
+    //     {
+    //       vars: "all",
+    //       varsIgnorePattern: "^_",
+    //       args: "after-used",
+    //       argsIgnorePattern: "^_",
+    //     },
+    //   ],
 
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        {
-          vars: "all",
-          varsIgnorePattern: "^_",
-          args: "after-used",
-          argsIgnorePattern: "^_",
-        },
-      ],
-    },
+    //   "@typescript-eslint/no-unused-vars": [
+    //     "warn",
+    //     {
+    //       vars: "all",
+    //       varsIgnorePattern: "^_",
+    //       args: "after-used",
+    //       argsIgnorePattern: "^_",
+    //     },
+    //   ],
+    // },
   },
 );
